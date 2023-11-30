561a9bc3410fa1ddc43cdddb55a2a0d7bbf3feaf
nasser faraji <faraji@bictory.io>
2023-11-30 12:35:01 +0330

    feat: payment servive

---
+++ libs/common/src/dto/card.dto.ts
@@ -0,0 +1,16 @@
+import { IsString, IsNotEmpty, IsNumber, IsCreditCard } from 'class-validator';
+
+export class CardDto {
+  @IsString()
+  @IsNotEmpty()
+  cvc: string;
+
+  @IsNumber()
+  exp_month: number;
+
+  @IsNumber()
+  exp_year: number;
+
+  @IsCreditCard()
+  number: string;
+}
