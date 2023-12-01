c08bdeed5b93f83263ec8fab6967ccd8af722151
nasser faraji <faraji@bictory.io>
2023-12-01 13:37:34 +0330

    feat: add payment for reservations

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -6,20 +6,21 @@ import { Stripe } from 'stripe';
 export class PaymentsService {
   constructor(private readonly configService: ConfigService) {}
   private readonly stripe = new Stripe(
-    this.configService.get('Stripe_Secret_key'),
+    this.configService.get('STRIPE_SECRET_KEY'),
     {
       apiVersion: '2023-10-16',
     },
   );
 
   async createCharge({ card, amount }: CreateChargeDto) {
-    const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card: card,
-    });
+    // const paymentMethod = await this.stripe.paymentMethods.create({
+    //   type: 'card',
+    //   card: card,
+    // });
 
     const paymentIntent = await this.stripe.paymentIntents.create({
-      payment_method: paymentMethod.id.toString(),
+      // payment_method: paymentMethod.id.toString(),
+      payment_method: 'pm_card_visa',
       amount: amount * 100,
       confirm: true,
       payment_method_types: ['card'],
